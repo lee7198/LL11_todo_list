eb808357301401d19c3592096d7c445525de7d64
lee7198 <lee7198@naver.com>
2023-02-10 15:30:12 +0900

    localStorage 값 useState로 저장

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import dateFormat, { masks } from "dateformat";
 
 function App() {
+  const [todo, setTodo] = useState(undefined);
+
+  const saveTodo = (e) => {
+    e.preventDefault();
+    const content = e.target.querySelector("#do").value;
+
+    setTodo((context) => [
+      ...context,
+      { createAt: new Date(), value: content, clear: false },
+    ]);
+
+    localStorage.setItem("to-do", JSON.stringify(todo));
+  };
+
+  useEffect(() => {
+    const todoStore = localStorage.getItem("to-do");
+    if (todoStore !== null) setTodo(JSON.parse(todoStore));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>To Do List</h1>
+      <div>
+        <form onSubmit={saveTodo}>
+          <input type="text" id="do" placeholder="Enter your List" />
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+      {todo && todo.map((list, index) => <MyList prop={list} key={index} />)}
+      {/* {todo && todo[0]} */}
+    </>
   );
 }
 
 export default App;
+
+const MyList = ({ prop }) => {
+  return (
+    <div>
+      {dateFormat(prop.createAt, "yy년 mm월 dd일 ")}
+      {prop.value}
+    </div>
+  );
+};
