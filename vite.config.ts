3803738fc18f85e38ae2aa7f78384e9aa036ac7a
lee7198 <lee7198@naver.com>
2023-02-23 17:41:32 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/LL11_todo_list/",
+  base: "/https://github.com/lee7198/LL11_todo_list.git/",
   plugins: [react()],
 });
